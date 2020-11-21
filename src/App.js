0498d9e0fed2438e49f769a63db289ae707f49a6
idesign3001 <idesign3001@gmail.com>
2020-11-21 15:35:27 +0900

    implemented firebase utils

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-import {auth} from '../src/firebase/firebase.utils';
+import {auth, createUserProfileDocument} from '../src/firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -23,11 +23,30 @@ class App extends React.Component {
   
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>  {
-      this.setState({currentUser:user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // this.setState({currentUser:user});
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-      console.log(this.state);
+        userRef.onSnapshot(snapShot => {
+
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          }, ()=> console.log(this.state));
+          console.log("this.state ", this.state);
+        });
+       
+      }
+      else {
+        this.setState({currentUser:userAuth});
+      }
+      // createUserProfileDocument(user);
+
+      // console.log(user);
+      // console.log(this.state);
     })
   }
 
